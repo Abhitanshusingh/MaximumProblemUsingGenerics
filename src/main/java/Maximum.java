0edf610e76c73be27f6d6399efe48e3c522f77b1
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-13 18:36:43 +0530

    ADDED: MaximumFloatValue method to find maximum value of float

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,7 +1,18 @@
 public class Maximum {
-    static Integer maximum = 0;
+    //LOGIC FOR FIND INTEGER MAXIMUM VALUE
     public static Integer findMaximumNumber(Integer[] intArray) {
+        Integer maximum = 0;
         for (Integer index : intArray) {
+            if (index.compareTo(maximum) > 0) {
+                maximum = index;
+            }
+        }
+        return maximum;
+    }
+    //LOGIC FOR FIND FLOAT MAXIMUM VALUE
+    public static Float MaximumFloatValue(Float[] intArray) {
+        Float maximum=0f;
+        for (Float index : intArray) {
             if (index.compareTo(maximum) > 0) {
                 maximum = index;
             }
