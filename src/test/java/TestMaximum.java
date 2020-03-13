c5fc6e992298513bef756f24221a01a4e954ce46
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-13 18:39:46 +0530

    ADDED: Finded maximum float value and passed test case

--- src/test/java/TestMaximum.java
+++ src/test/java/TestMaximum.java
@@ -10,4 +10,10 @@ public class TestMaximum {
         Integer intArray[] = {10, 20, 30};
         Assert.assertEquals(intArray[2],Maximum.findMaximumNumber(intArray));
     }
+    @Test
+    public void givenThreeFloat_WhenProper_ShouldReturnMaximum() {
+        value = new Maximum();
+        Float intArray[] = {40.0f, 5.0f, 30.f};
+        Assert.assertEquals(intArray[0],Maximum.MaximumFloatValue(intArray));
+    }
 }
