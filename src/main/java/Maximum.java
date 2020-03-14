fb9ee8ba24eeeac80d2ec996671643f4a00c0f86
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-14 18:50:35 +0530

    ADDED: Printed maximum value

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -2,12 +2,14 @@ public class Maximum<E extends Comparable> {
     E firstVariable;
     E secondVariable;
     E thirdVariable;
+
     //DECLEARED PARAMETERIZED CONSTRUCTOR
     Maximum(E firstVariable, E secondVariable, E thirdVariable) {
         this.firstVariable = firstVariable;
         this.secondVariable = secondVariable;
         this.thirdVariable = thirdVariable;
     }
+
     //LOGIC FOR FIND MAXIMUM VALUE
     public <E extends Comparable> E maximumValue(E firstVariable, E secondVariable, E thirdVariable) {
 
@@ -18,8 +20,15 @@ public class Maximum<E extends Comparable> {
         else
             return (E) thirdVariable;
     }
+
     //CALLING MAXIMUMVALUE AND TOPRINT METHOD AND ALSO RETURN MAX VALUE
     public E maximumValue() {
+        toPrint(maximumValue(firstVariable, secondVariable, thirdVariable));
         return maximumValue(firstVariable, secondVariable, thirdVariable);
+    }
+
+    //PRINTING MAXIMUM VALUE
+    public <E extends Comparable> void toPrint(E maximum) {
+        System.out.println("Maximum value is :" + maximum);
     }
 }
