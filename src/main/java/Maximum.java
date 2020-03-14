13143fb551634d6774a05b14176541e71e2540b1
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-14 15:24:47 +0530

    REFACTORED: Create parameterized constructor initialize three variable and finded maximum

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,12 +1,23 @@
-public class Maximum {
+public class Maximum<E extends Comparable> {
+    E firstVariable;
+    E secondVariable;
+    E thirdVariable;
+
+    //DECLEARED PARAMETERIZED CONSTRUCTOR
+    Maximum(E firstVariable, E secondVariable, E thirdVariable) {
+        this.firstVariable = firstVariable;
+        this.secondVariable = secondVariable;
+        this.thirdVariable = thirdVariable;
+    }
+
     //LOGIC FOR FIND MAXIMUM VALUE
-    public static <E extends Comparable> E maximumValue(E[] Array) {
-        E maximum = Array[0];
-        for (E index : Array) {
-            if (index.compareTo(maximum) > 0) {
-                maximum = index;
-            }
-        }
-        return maximum;
+    public <E extends Comparable> E maximumValue() {
+
+        if (firstVariable.compareTo(secondVariable) > 0 && firstVariable.compareTo(thirdVariable) > 0)
+            return (E) firstVariable;
+        else if (secondVariable.compareTo(firstVariable) > 0 && secondVariable.compareTo(thirdVariable) > 0)
+            return (E) secondVariable;
+        else
+            return (E) thirdVariable;
     }
 }
