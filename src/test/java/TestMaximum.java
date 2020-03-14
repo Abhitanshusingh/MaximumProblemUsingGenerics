34b36aeea11ca29953a65a8190d01b9c3206ffe8
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-14 18:38:14 +0530

    REFACTORED: Calling maximumValue method

--- src/test/java/TestMaximum.java
+++ src/test/java/TestMaximum.java
@@ -6,53 +6,64 @@ public class TestMaximum {
 
     @Test
     public void givenThreeInteger_WhenProper_ShouldReturnMaximum1() {
-        value = new Maximum(10,20,30);
-        Assert.assertEquals(30, value.maximumValue());
+        value = new Maximum(10, 20, 30);
+        Integer maxInteger = (Integer) value.maximumValue();
+        Assert.assertEquals((Integer) 30, maxInteger);
     }
+
     @Test
     public void givenThreeInteger_WhenProper_ShouldReturnMaximum2() {
-        value = new Maximum(40,30,20);
-        Assert.assertEquals(40, value.maximumValue());
+        value = new Maximum(40, 30, 20);
+        Integer maxInteger = (Integer) value.maximumValue();
+        Assert.assertEquals((Integer) 40, maxInteger);
     }
+
     @Test
     public void givenThreeInteger_WhenProper_ShouldReturnMaximum3() {
-        value = new Maximum(20,10,30);
-        Assert.assertEquals(30, value.maximumValue());
+        value = new Maximum(20, 10, 50);
+        Integer maxInteger = (Integer) value.maximumValue();
+        Assert.assertEquals((Integer) 50, maxInteger);
     }
 
     @Test
     public void givenThreeFloat_WhenProper_ShouldReturnMaximum1() {
         value = new Maximum(40.0f, 5.0f, 30.f);
-        Assert.assertEquals(40.0f, value.maximumValue());
+        Float maxFloat = (Float) value.maximumValue();
+        Assert.assertEquals((Float) 40.0f, maxFloat);
     }
 
     @Test
     public void givenThreeFloat_WhenProper_ShouldReturnMaximum2() {
         value = new Maximum(5.0f, 40.0f, 30.f);
-        Assert.assertEquals(40.0f, value.maximumValue());
+        Float maxFloat = (Float) value.maximumValue();
+        Assert.assertEquals((Float) 40.0f, maxFloat);
     }
 
     @Test
     public void givenThreeFloat_WhenProper_ShouldReturnMaximum3() {
         value = new Maximum(30.0f, 5.0f, 40.f);
-        Assert.assertEquals(40.0f, value.maximumValue());
+        Float maxFloat = (Float) value.maximumValue();
+        Assert.assertEquals((Float) 40.0f, maxFloat);
     }
 
     @Test
     public void givenThreeString_WhenProper_ShouldReturnMaximum1() {
-        value = new Maximum( "modi", "trump", "zabhi");
-        Assert.assertEquals("zabhi", value.maximumValue());
+        value = new Maximum("modi", "trump", "zabhi");
+        String maxString = (String) value.maximumValue();
+        Assert.assertEquals("zabhi", maxString);
     }
 
     @Test
     public void givenThreeString_WhenProper_ShouldReturnMaximum2() {
-        value = new Maximum( "zmodi", "10", "trump");
-        Assert.assertEquals("zmodi", value.maximumValue());
+        value = new Maximum("zmodi", "10", "trump");
+        String maxString = (String) value.maximumValue();
+        Assert.assertEquals("zmodi", maxString);
     }
 
     @Test
     public void givenThreeString_WhenProper_ShouldReturnMaximum3() {
-        value = new Maximum( "20.20", "zmodi", "trump");
-        Assert.assertEquals("zmodi", value.maximumValue());
+        value = new Maximum("20.20", "zmodi", "trump");
+        String maxString = (String) value.maximumValue();
+        Assert.assertEquals("zmodi", maxString);
     }
 }
