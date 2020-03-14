13e721d8262961129b69263353f03f2dae7e4ac5
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-14 12:56:38 +0530

    REFACTORED: all 3 method to one generic method and find the maximum

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,30 +1,8 @@
 public class Maximum {
-    //LOGIC FOR FIND INTEGER MAXIMUM VALUE
-    public static Integer findMaximumNumber(Integer[] intArray) {
-        Integer maximum = 0;
-        for (Integer index : intArray) {
-            if (index.compareTo(maximum) > 0) {
-                maximum = index;
-            }
-        }
-        return maximum;
-    }
-
-    //LOGIC FOR FIND FLOAT MAXIMUM VALUE
-    public static Float MaximumFloatValue(Float[] intArray) {
-        Float maximum = 0f;
-        for (Float index : intArray) {
-            if (index.compareTo(maximum) > 0) {
-                maximum = index;
-            }
-        }
-        return maximum;
-    }
-
-    //LOGIC FOR FIND STRING MAXIMUM VALUE
-    public static String MaximumStringValue(String[] intArray) {
-        String maximum = "";
-        for (String index : intArray) {
+    //LOGIC FOR FIND MAXIMUM VALUE
+    public static <E extends Comparable> E maximumValue(E[] Array) {
+        E maximum = Array[0];
+        for (E index : Array) {
             if (index.compareTo(maximum) > 0) {
                 maximum = index;
             }
