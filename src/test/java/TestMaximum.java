fe30a7a339106106ccbcd3cf4ee4199cea95258c
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-14 15:27:52 +0530

    REFACTORED: Define test case of integer float and string and passed test case

--- src/test/java/TestMaximum.java
+++ src/test/java/TestMaximum.java
@@ -5,23 +5,54 @@ public class TestMaximum {
     Maximum value;
 
     @Test
-    public void givenThreeInteger_WhenProper_ShouldReturnMaximum() {
-        value = new Maximum();
-        Integer intArray[] = {10, 20, 30};
-        Assert.assertEquals(intArray[2], Maximum.findMaximumNumber(intArray));
+    public void givenThreeInteger_WhenProper_ShouldReturnMaximum1() {
+        value = new Maximum(10,20,30);
+        Assert.assertEquals(30, value.maximumValue());
+    }
+    @Test
+    public void givenThreeInteger_WhenProper_ShouldReturnMaximum2() {
+        value = new Maximum(40,30,20);
+        Assert.assertEquals(40, value.maximumValue());
+    }
+    @Test
+    public void givenThreeInteger_WhenProper_ShouldReturnMaximum3() {
+        value = new Maximum(20,10,30);
+        Assert.assertEquals(30, value.maximumValue());
     }
 
     @Test
-    public void givenThreeFloat_WhenProper_ShouldReturnMaximum() {
-        value = new Maximum();
-        Float floatArray[] = {40.0f, 5.0f, 30.f};
-        Assert.assertEquals(floatArray[0], Maximum.MaximumFloatValue(floatArray));
+    public void givenThreeFloat_WhenProper_ShouldReturnMaximum1() {
+        value = new Maximum(40.0f, 5.0f, 30.f);
+        Assert.assertEquals(40.0f, value.maximumValue());
     }
 
     @Test
-    public void givenThreeString_WhenProper_ShouldReturnMaximum() {
-        value = new Maximum();
-        String stringArray[] = {"Z", "A", "C"};
-        Assert.assertEquals(stringArray[0], Maximum.MaximumStringValue(stringArray));
+    public void givenThreeFloat_WhenProper_ShouldReturnMaximum2() {
+        value = new Maximum(5.0f, 40.0f, 30.f);
+        Assert.assertEquals(40.0f, value.maximumValue());
+    }
+
+    @Test
+    public void givenThreeFloat_WhenProper_ShouldReturnMaximum3() {
+        value = new Maximum(30.0f, 5.0f, 40.f);
+        Assert.assertEquals(40.0f, value.maximumValue());
+    }
+
+    @Test
+    public void givenThreeString_WhenProper_ShouldReturnMaximum1() {
+        value = new Maximum( "modi", "trump", "zabhi");
+        Assert.assertEquals("zabhi", value.maximumValue());
+    }
+
+    @Test
+    public void givenThreeString_WhenProper_ShouldReturnMaximum2() {
+        value = new Maximum( "zmodi", "10", "trump");
+        Assert.assertEquals("zmodi", value.maximumValue());
+    }
+
+    @Test
+    public void givenThreeString_WhenProper_ShouldReturnMaximum3() {
+        value = new Maximum( "20.20", "zmodi", "trump");
+        Assert.assertEquals("zmodi", value.maximumValue());
     }
 }
